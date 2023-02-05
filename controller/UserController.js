e7c63aa1ed4d5c0bb9f0032825c79bcfabc3d640
ozen-hub <info.ozenhub@gmail.com>
2023-02-05 21:33:46 +0530

    3

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,17 +1,38 @@
-// save,user=>email,-> welcome -> (web)
+const nodemailer = require("nodemailer");
 const UserSchema= require('../model/User');
-const registerUser=(req,resp)=>{
+const registerUser=  (req,resp)=>{
     const user = new UserSchema({
        fullName:req.body.fullName,
        email:req.body.email,
        password:req.body.password
     });
-    user.save().then(result=>{
+    user.save().then(async result=>{
        let responseObject={
            message:'user created',
            email:result.email,
            token:'token'
        }
+
+        let transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.SENDER_EMAIL,
+                pass: process.env.EMAIL_APP_KEY
+            }
+        });
+
+        await transporter.sendMail({
+            from: '"Fred Foo 👻" <education.seekerscloud@gmail.com>', // sender address
+            to: req.body.email, // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: "Registration completed", // plain text body
+            html: "<b>Registration completed!</b>", // html body
+        });
+
+
+
         resp.status(201).json(responseObject);
     }).catch(error=>{
         resp.status(500).json(error);
