3dcc188c07c1d0d09bfcdefb06e9c144365c7987
ozen-hub <info.ozenhub@gmail.com>
2023-02-12 21:33:07 +0530

    9

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,17 +1,33 @@
 const OrderSchema = require('../model/Order');
-const createOrder =(req,resp)=>{
+const jwt = require('jsonwebtoken');
+const createOrder = (req, resp) => {
     const order = new OrderSchema({
-        cost:req.body.cost,
-        date:req.body.date,
-        customer:req.body.customer,
-        products:req.body.products
+        cost: req.body.cost,
+        date: req.body.date,
+        customer: req.body.customer,
+        products: req.body.products
     });
-    order.save().then(result=>{
-        resp.status(201).json(result);
-    }).catch(error=>{
-        resp.status(500).json(error);
-    })
+
+    let token = req.header('authorization');
+    if (token.toString().startsWith('Bearer ')) {
+        token = token.toString().replace('Bearer ', '');
+
+        jwt.verify(token, process.env.PRIVATE_KEY, function (err, decoded) {
+            if (err) {
+                resp.status(503).json({'message': 'Wrong token'});
+                return;
+            }
+            order.save().then(result => {
+                resp.status(201).json(result);
+            }).catch(error => {
+                resp.status(500).json(error);
+            })
+        });
+
+    } else {
+        resp.status(401).json({'message': 'Wrong token'});
+    }
 }
-module.exports={
+module.exports = {
     createOrder
 }
